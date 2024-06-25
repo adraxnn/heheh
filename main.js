2abf49caf3d051f0535c06e0576a1356afe5d22b
adraxnn <reywayst@gmail.com>
2024-06-25 17:29:06 +0700

    main.js

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
 onload = () =>{
         document.body.classList.remove("container");
 };
-
-window.addEventListener('load', function() {
-        var audio = document.getElementById('myAudio');
-        audio.play();
-    });
